ed9fde2c68e849aa7bd0f00e698c7d4fd62795ab
mtomic <mtomic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 21:03:00 +0000

    retag v4.1.0
    git-svn-id: https://plugins.svn.wordpress.org/smsify/tags/4.1.0@855059 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/views/smsify-reporting.php
@@ -0,0 +1,46 @@
+<?php if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+    exit ("Do not access this file directly.");
+?>
+<?php 
+//TODO Add Users array loop.
+?>
+<h2><?php _e("SMS Usage in Year " . $selected_year . " by month") ?></h2>
+<form action="" method="get">
+    <input type="hidden" name="page" value="smsify-reporting">
+    <div class="tablenav top">
+        <div class="alignleft">
+            <label for="year">Year        
+                <select id="year" name="year">
+                    <option value="<?php echo $current_year+1 ?>"<?php if($current_year+1 == $selected_year) {echo ' selected="selected"';} ?>><?php echo $current_year+1 ?></option>
+                    <option value="<?php echo $current_year ?>"<?php if($current_year == $selected_year) {echo ' selected="selected"';} ?>><?php echo $current_year ?></option>
+                    <?php for($i=-1; $i > -2; $i--) : ?>
+                        <option value="<?php echo $current_year+$i ?>"<?php if($current_year+$i == $selected_year) {echo ' selected="selected"';} ?>><?php echo $current_year+$i ?></option>
+                    <?php endfor; ?>
+                </select>
+            </label>
+            <label for="user_id">User:        
+                <select id="user_id" name="user_id">
+                    <option value=""><?php _e("All Users"); ?></option>
+                    <?php foreach ($users as $user) : ?>
+                        <option value="<?php echo $user->ID ?>"<?php if($user->ID == $_GET['user_id']){echo ' selected="selected"';} ?>><?php echo $user->user_login ?></option>
+                    <?php endforeach ?>
+                </select>
+            </label>
+            <input type="submit" name="" id="doaction" class="button action" value="SHOW REPORT">
+        </div>
+    </div>
+</form>
+<table class="wp-list-table widefat fixed">
+    <tbody>
+        <?php foreach($stats->$selected_year as $month_num => $total) : ?>
+            <tr class="alternate"<?php if($month_num % 2 == 0) { echo ' style="background:#eee"'; } ?>>
+                <td scope="row"><?php echo date('F', mktime(0, 0, 0, $month_num, 10)); ?></label></td>
+                <td><?php echo $total; $grandtotal += $total; ?></td>
+            </tr>
+        <?php endforeach ?>
+            <tr>
+                <th scope="row"><strong><?php _e("Total"); ?></strong></th>
+                <td><strong><?php echo $grandtotal ?></strong></td>
+            </tr>
+    </tbody>
+</table>
---
+++ trunk/views/smsify-send-user.php
@@ -0,0 +1,109 @@
+<?php if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+    exit ("Do not access this file directly.");
+?>
+<?php if($smsify_params->api_key) : ?>
+    <h3><?php _e("SMSify Settings")?></h3>
+    <div class="smsify-send">
+        <div class="smsify-sending"><img src="<?php echo $smsify_params->imageurl ?>/loading-image.gif" alt="loading..." /><p><?php _e("Sending SMS"); ?></p></div>
+        <div class="smsify-confirmation update-nag"></div>
+        <table class="form-table">
+            <tbody>
+                <tr>
+                    <th><label for="smsify_mobile"><?php _e("Mobile number"); ?></label></th>
+                    <td>
+                        <input type="number" name="smsify_mobile" id="smsify_mobile" value="<?php echo esc_attr( get_the_author_meta( 'smsify_mobile', $user->ID ) ); ?>" class="regular-text" maxlength="15" /><br />
+                        <span class="description"><?php _e("Please enter user's mobile number in international format.<br/> E.g. 61414111111, where 61 is a country code for Australia.<br/>Only numbers are allowed, no spaces or + signs please."); ?></span>
+                    </td>
+                </tr>
+                <tr>
+                    <th><label for="smsify_message"><?php _e("Message"); ?></label></th>
+                    <td>
+                        <textarea name="smsify_message" id="smsify_message" rows="5" maxlength="160"><?php echo esc_attr( get_the_author_meta( 'smsify_message', $user->ID ) ); ?></textarea>
+                        <br/>
+                        <span class="description"><?php _e("Maximum 160 characters."); ?></span>
+                    </td>
+                </tr>
+                <?php if(get_site_option('smsify-enable-sender-id-override')) : ?>
+                <tr>
+                    <th><label for="smsify_sender_id"><?php _e("Sender ID"); ?></label></th>
+                    <td>
+                        <input type="number" name="smsify_sender_id" id="smsify_sender_id" value="<?php echo esc_attr( get_the_author_meta( 'smsify_sender_id', $user->ID ) ); ?>" class="regular-text" maxlength="15" /><br />
+                        <span class="description"><?php _e("If you purchased additional Sender ID(s), enter it here. Use this with caution. If your SenderID is incorrect, the message will not get delivered and you may be charged for SMS credit(s). Leave blank to use default Sender ID."); ?></span>
+                    </td>
+                </tr>
+                <?php endif; ?>
+                <tr>
+                    <th><label for="smsify-scheduler"><?php _e("Schedule SMS"); ?></label></th>
+                    <td><input type="checkbox" name="smsify-scheduler" id="smsify-scheduler" value="1" /></td>
+                </tr>
+                <tr id="smsify-scheduleblock1">
+                    <th><label for="smsify-schedule-date"><?php _e("Schedule Date"); ?></label></th>
+                    <td>
+                        <input type="text" name="smsify-schedule-date" id="smsify-schedule-date" readonly="readonly" />
+                    </td>
+                </tr>
+                <tr id="smsify-scheduleblock2">
+                    <th><label for="smsify-schedule-time"><?php _e("Schedule Time"); ?></label></th>
+                    <td>
+                        <select name="smsify-schedule-time" id="smsify-schedule-time">
+                            <option value="">-- Please select --</option>
+                            <option value="0">12:00 AM</option>
+                            <option value="30">12:30 AM</option>
+                            <option value="60">01:00 AM</option>
+                            <option value="90">01:30 AM</option>
+                            <option value="120">02:00 AM</option>
+                            <option value="150">02:30 AM</option>
+                            <option value="180">03:00 AM</option>
+                            <option value="210">03:30 AM</option>
+                            <option value="240">04:00 AM</option>
+                            <option value="270">04:30 AM</option>
+                            <option value="300">05:00 AM</option>
+                            <option value="330">05:30 AM</option>
+                            <option value="360">06:00 AM</option>
+                            <option value="390">06:30 AM</option>
+                            <option value="420">07:00 AM</option>
+                            <option value="450">07:30 AM</option>
+                            <option value="480">08:00 AM</option>
+                            <option value="510">08:30 AM</option>
+                            <option value="540">09:00 AM</option>
+                            <option value="570">09:30 AM</option>
+                            <option value="600">10:00 AM</option>
+                            <option value="630">10:30 AM</option>
+                            <option value="660">11:00 AM</option>
+                            <option value="690">11:30 AM</option>
+                            <option value="720">12:00 PM</option>
+                            <option value="750">12:30 PM</option>
+                            <option value="780">01:00 PM</option>
+                            <option value="810">01:30 PM</option>
+                            <option value="840">02:00 PM</option>
+                            <option value="870">02:30 PM</option>
+                            <option value="900">03:00 PM</option>
+                            <option value="930">03:30 PM</option>
+                            <option value="960">04:00 PM</option>
+                            <option value="990">04:30 PM</option>
+                            <option value="1020">05:00 PM</option>
+                            <option value="1050">05:30 PM</option>
+                            <option value="1080">06:00 PM</option>
+                            <option value="1110">06:30 PM</option>
+                            <option value="1140">07:00 PM</option>
+                            <option value="1170">07:30 PM</option>
+                            <option value="1200">08:00 PM</option>
+                            <option value="1230">08:30 PM</option>
+                            <option value="1260">09:00 PM</option>
+                            <option value="1290">09:30 PM</option>
+                            <option value="1320">10:00 PM</option>
+                            <option value="1350">10:30 PM</option>
+                            <option value="1380">11:00 PM</option>
+                            <option value="1410">11:30 PM</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2"><button class="button button-secondary smsify-sendsms"><?php _e("SEND SMS"); ?></button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+<?php else : ?>
+    <div class="error"><p><?php _e("You must activte SMSify before you can start sending SMS to this user. <a href='admin.php?page=smsify-settings'>Activate SMSify now.</a>"); ?></p></div>
+<?php endif ?>

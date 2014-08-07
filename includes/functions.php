e2b49bc0dfe3ec3672e3de7bb23f6d12fa78fe3b
mtomic <mtomic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-07 11:19:36 +0000

    Fix header warnings
    git-svn-id: https://plugins.svn.wordpress.org/smsify/tags/4.1.4@961653 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/functions.php
+++ includes/functions.php
@@ -5,7 +5,7 @@ function smsify_getConfig() {
     global $smsify_params;
     global $current_user;
     $smsify_params = new stdClass();
-    $smsify_params->appVersion = '4.1.0';
+    $smsify_params->appVersion = '4.1.4';
     $smsify_params->api_key = get_site_option('smsify-api-key');
     $smsify_params->apiprotocol = 'https';
     $smsify_params->apihost = 'www.smsify.com.au';
@@ -52,27 +52,27 @@ function smsify_sms_handler() {
     if ( !current_user_can( 'edit_user', $user_id ) ) { die("Invalid request - 000"); }
     
     if(!isset($_POST['send_to'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 002");
     }
     
     if(!isset($_POST['message'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 003");
     }
     
     if(!isset($_POST['user_id'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 004");
     }
     
     if(!isset($_POST['scheduler'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 005");
     }
     
     if(!isset($_POST['schedule_date_time'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 006");
     }
     
@@ -181,22 +181,22 @@ function smsify_sms_group_handler() {
     if ( !current_user_can( 'edit_user', $user_id ) ) { die("Invalid request - 000"); }
     
     if(!isset($_POST['message'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 001");
     }
     
     if(!isset($_POST['tag_id'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 002");
     }
     
     if(!isset($_POST['scheduler'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 003");
     }
     
     if(!isset($_POST['schedule_date_time'])) {
-        header("HTTP/1.1 500 Invalid Request");
+        //header("HTTP/1.1 500 Invalid Request");
         die("Invalid request - 004");
     }
     
